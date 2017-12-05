8e3ca4d871feeede000c40df9aa8433d093072a4
Jeremy Evans <openspacehexagon@gmail.com>
2017-12-05 13:14:46 -0500

    setting up parser

--- ast/ast.go
+++ ast/ast.go
@@ -1,30 +1,31 @@
 package ast
 
-import "monkey/token"
+import "github.com/SpaceHexagon/ecs/token"
 
-type Program struct { 
+type Program struct {
 	Statements []Statement
 }
 
-func (p *Program) TokenLiteral() string { 
+func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
-		return "" 
+		return ""
 	}
 }
 
-type Node interface { TokenLiteral() string
+type Node interface {
+	TokenLiteral() string
 }
 
-type Statement interface { 
+type Statement interface {
 	Node
-    statementNode()
+	statementNode()
 }
 
-type Expression interface { 
+type Expression interface {
 	Node
-    expressionNode()
+	expressionNode()
 }
 
 type LetStatement struct {
@@ -33,7 +34,7 @@ type LetStatement struct {
 }
 
 func (ls *LetStatement) statementNode() {}
-func (ls *LetStatement) TokenLiteral() string { 
+func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
@@ -42,6 +43,6 @@ type Identifier struct {
 }
 
 func (i *Identifier) expressionNode() {}
-func (i *Identifier) TokenLiteral() string { 
-	return i.Token.Literal 
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
